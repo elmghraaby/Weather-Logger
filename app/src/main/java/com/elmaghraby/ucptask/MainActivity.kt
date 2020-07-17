994192e0c2ea92ea7ede3b728a18466f6c06f7ff
Ahmed Alaa <ahmed.alaa50.aa@gmail.com>
2020-07-17 15:24:16 +0200

    Add files.

---
+++ app/src/main/java/com/elmaghraby/ucptask/MainActivity.kt
@@ -0,0 +1,15 @@
+/******************************************************************************
+ *  Copyright (c) 2020, Created By Ahmed Alaa Elmaghraby.                     *
+ ******************************************************************************/
+
+package com.elmaghraby.ucptask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
